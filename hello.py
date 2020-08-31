ebefe7512e099bd33cff4a2c10dae793ad6182bc
ankeshkr <32651031+ankeshkr@users.noreply.github.com>
2020-09-01 00:34:28 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
+import pickle
 def answer(name):
   print("Hello" + name)
 
 a=input("what is your name")
 print (a)
 answer(a)
+pickle.dump(answer, open('model.pkl','wb'))
+
+# Loading model to compare the results
+model = pickle.load(open('model.pkl','rb'))
